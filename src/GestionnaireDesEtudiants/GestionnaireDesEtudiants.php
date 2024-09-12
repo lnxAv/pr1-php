3fc0bbdd8a947676d0f6353550c40e8df92cde6a
Levi Loseke <157930732+CaptnLevi96@users.noreply.github.com>
2024-09-12 17:22:04 -0400

    GestionnaireDesEtudiants

--- src/GestionnaireDesEtudiants/GestionnaireDesEtudiants.php
+++ src/GestionnaireDesEtudiants/GestionnaireDesEtudiants.php
@@ -11,25 +11,93 @@ abstract class GestionnaireDesEtudiants extends OperationsEtudiantes {
     abstract protected function toString();
     abstract protected function fromString();
 
+        // protected function close
+
+
     protected function close() {
         if ($this->file) {
             fclose($this->file);
         }
     }
 
+        // protected function open
+
+
     protected function open($state) {
         $this->close(); 
         $this->file = fopen($this->filePath, $state);
     }
 
-    protected function push() {
+    // protected function push
+
+
+    protected function push($data) {
         $this->open("w"); 
-        fwrite($this->file);
-        fclose($this->file); 
+        fwrite($this->file, $data);
+        $this->close(); 
+    }
+
+
+// protected function replace
+
+
+    protected function replace(array $comparators, bool $reduce = false) {
+        $this->open("r+");
+        $tempFile = tempnam(sys_get_temp_dir(), 'temp');
+        $temp = fopen($tempFile, "w");
+        while (($line = fgets($this->file)) !== false) {
+            $replaced = false;
+            foreach ($comparators as $key => $comparator) {
+                if ($comparator($line)) {
+                    $newLine = $this->toString();
+                    fwrite($temp, $newLine . PHP_EOL);
+                    $replaced = true;
+                    if ($reduce) {
+                        unset($comparators[$key]);
+                    }
+                    break;
+                }
+            }
+            if (!$replaced) {
+                fwrite($temp, $line);
+            }
+        }
+        fclose($temp);
+        $this->close();
+        unlink($this->filePath);
+        rename($tempFile, $this->filePath);
+    }
+
+    // protected function remove
+
+
+    protected function remove(array $comparators, bool $reduce = false) {
+        $this->open("r");
+        $tempFile = tempnam(sys_get_temp_dir(), 'temp');
+        $tempHandle = fopen($tempFile, "w");
+
+        while (($line = fgets($this->file)) !== false) {
+            $shouldRemove = false;
+            foreach ($comparators as $key => $comparator) {
+                if ($comparator($line)) {
+                    $shouldRemove = true;
+                    if ($reduce) {
+                        unset($comparators[$key]);
+                    }
+                    break;
+                }
+            }
+            if (!$shouldRemove) {
+                fwrite($tempHandle, $line);
+            }
+        }
+        fclose($tempHandle);
+        $this->close();
+        unlink($this->filePath);
+        rename($tempFile, $this->filePath);
     }
 }
 ?>
-
 
 Class to manipulate the file
 /* -variable
--- src/GestionnaireDesEtudiants/GestionnaireFichiersEtudiants.php
+++ src/GestionnaireDesEtudiants/GestionnaireFichiersEtudiants.php
@@ -20,5 +20,52 @@ class GestionnaireDesFichiersEtudiants extends GestionnaireDesEtudiants {
   
     }
 }
-
 ?>
+
+__construct(filePath){
+  $this->filePath = filePath
+}
+
+/* private function fromString()
+* @input  - a string representing a line inside the file
+* @desc   - takes in a string and format it as a student
+* @return - Object Student
+*/
+
+/* private function toString()
+* @input  - an object Student
+* @desc   - takes a object students and format it as a string
+* @return - string
+*/
+
+/* public function getStudents
+* @input  - input email(optional)
+* @desc   - access the file,
+* if email, return specific student
+* else return all students
+* @return - an array students or a single student
+*/
+
+/* public function setStudents
+* @input  - object Student
+* @desc   - add student to the file
+* if exist don't add
+* else add new student
+* @return - true if added, else false
+* /
+
+/* public function putStudents
+* @input  - object student
+* @desc   - take in existing student
+* if email exits update
+* else don't update
+* @return - true if updated, else false
+*/
+
+/* public function deleteStudentsByEmail
+* @input  - email
+* @desc   - take in existing email
+* if email exits delete
+* else don't delete
+* @return - true if deleted, else false
+*/
